d31c4272c3345128c334320b77e14bbadec0786b
alessandrofuda <alessandro.fuda@gmail.com>
2017-11-10 11:56:57 +0000

    Get/post API call with axios library - First method

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,7 @@
         <h1>Your IP is {{ ip }}</h1>
         <input type="text" v-model="input.firstname" placeholder="First Name" />
         <input type="text" v-model="input.lastname" placeholder="Last Name" />
-        <button>Send</button>
+        <button v-on:click="sendData()">Send</button>
         <br />
         <br />
         <textarea>{{ response }}</textarea>
@@ -25,8 +25,22 @@
                 response: ""
             }
         },
-        mounted() { },
-        methods: { }
+        mounted() {
+            axios({ method: "GET", "url": "https://httpbin.org/ip" }).then(result => {
+                this.ip = result.data.origin;
+            }, error => {
+                console.error(error);
+            });
+        },
+        methods: {
+            sendData() {
+                axios({ method: "POST", "url": "https://httpbin.org/post", "data": this.input, "headers": { "content-type": "application/json" } }).then(result => {
+                    this.response = result.data;
+                }, error => {
+                    console.error(error);
+                });
+            }
+        }
     }
 </script>
 
